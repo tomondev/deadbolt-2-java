2d802e442edc973c3a35a98becaaa719ee0a72ea
Thomas Koeppen <tomondev@gmail.com>
2013-02-11 21:47:59 +0100

    play 2.1.0

--- project/Build.scala
+++ project/Build.scala
@@ -5,11 +5,11 @@ import play.Project._
 object ApplicationBuild extends Build {
 
   val appName         = "deadbolt-java"
-  val appVersion      = "2.1-SNAPSHOT"
+  val appVersion      = "2.1.0"
 
   val appDependencies = Seq(
     javaCore,
-    "be.objectify" %% "deadbolt-core" % "2.1-SNAPSHOT"
+    "be.objectify" %% "deadbolt-core" % "2.1.0"
   )
 
   val main = play.Project(appName, appVersion, appDependencies).settings(
